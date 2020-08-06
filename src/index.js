e3018935002881b0e033c2cddad7d645ce69110d
Pakornsak Supamongkonchaikul <pakornsak.sup@ascendcorp.com>
2020-08-06 16:41:51 +0700

    Add Object feature

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ const mathFeature = require("./feature/math");
 const numberFeature = require("./feature/number");
 const regexpFeature = require("./feature/regexp");
 const stringFeature = require("./feature/string");
+const objectFeature = require("./feature/object");
 
 /**
  * @param {number} v
  */
 const chrome = (v) => {
+  // remove features
   arrayFeature(v);
   dateFeature(v);
   mathFeature(v);
   numberFeature(v);
+  objectFeature(v);
   regexpFeature(v);
   stringFeature(v);
 };
